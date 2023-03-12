6e78c8e44931992eb8b33b69b68cffb5ee6cbfc5
James Niyongira <lascasasjames85@gmail.com>
2023-03-11 19:45:07 -0500

    project initialization

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import ImageList from "./ImageList";
+import SearchBar from "./SearchBar";
+import ImageShow from "./ImageShow";
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ImageList />
+      <SearchBar />
+      <ImageShow />
+    </div>
+  );
+}
+
+export default App;
